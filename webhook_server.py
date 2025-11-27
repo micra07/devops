9847086653658a1d9cbbcad3d135290e818ae62a
micra07 <126266110+micra07@users.noreply.github.com>
2025-11-27 14:24:15 +0400

    Update webhook_server.py

--- webhook_server.py
+++ webhook_server.py
@@ -3,7 +3,7 @@
 Webhook сервер для автоматического развертывания catty-reminders-app
 Использует FastAPI вместо BaseHTTPRequestHandler
 """
-# morning test7
+# morning test8
 from fastapi import FastAPI, Request, Response
 import subprocess
 import os
